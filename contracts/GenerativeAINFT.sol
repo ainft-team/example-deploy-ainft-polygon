11fe21a6756d0867a869b34c677ec19ce4550d72
Jungwoo Pyo <jungwoo_pyo@comcom.ai>
2023-03-29 15:35:29 +0900

    feat: sample contract implemented

--- contracts/GenerativeAINFT.sol
+++ contracts/GenerativeAINFT.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
+/**
+ * @notice This contract is temporary and will be changed after the AINFT template contract is completed.
+ * Never use this in production level.
+*/
 contract GenerativeAINFT is ERC721URIStorage, Ownable {
     string public baseURI;
     constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {}
@@ -18,7 +21,8 @@ contract GenerativeAINFT is ERC721URIStorage, Ownable {
     function setBaseURI(string memory newBaseURI) public onlyOwner {
         baseURI = newBaseURI;
     }
-    function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(_ownerOf(tokenId) == _msgSender(), "The holder only changes or updates the URI.");
         _setTokenURI(tokenId, _tokenURI);
     }
 }
